788a387e73706607b84bda1d20ebc564c589c0d7
notfound313 <marufmuh666@gmail.com>
2024-07-09 14:36:47 +0700

    Constructor Class

---
+++ ConstructureClass/Program.cs
@@ -0,0 +1,45 @@
+﻿class Home 
+{
+	public int door;
+	public int window;
+	public  string colour;
+	public string address;
+	//constructor paramaterless
+	public Home()
+	{
+		Console.WriteLine($"Home Created : {door} {window} {colour} {address}");
+		
+	}
+	// constructor parameter
+	public Home(int door, int window, string colour, string address)
+	{
+		this.door = door;
+		this.window = window;
+		this.colour = colour;
+		this.address = address;
+		Console.WriteLine($"Home Created : {door} {window} {colour} {address}");
+	}
+	
+	public Home(int door, int window)
+	{
+		this.door = door;
+		this.window = window;
+		Console.WriteLine($"Home Created : {door} {window}");
+		
+	}
+	
+	
+}
+
+class Program
+{
+	static void Main()
+	{
+		Home home = new Home();
+		Home home1 = new Home(2,3,"black", "Bandung");
+		Home home2 = new Home(2, 4);
+		
+		
+	}
+	 
+}
