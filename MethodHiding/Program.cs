ce68697e56779fd94b50a835a946e32e8bc150c4
notfound313 <marufmuh666@gmail.com>
2024-07-11 15:02:08 +0700

    OverLoading

---
+++ MethodHiding/Program.cs
@@ -0,0 +1,33 @@
+﻿class Parent
+{
+	// public void Greating()
+	// {
+	// 	Console.WriteLine("tidak boleh");
+	// }
+	public virtual void Greating()
+	{
+		Console.WriteLine("tidak boleh");
+	}
+}
+
+class Child : Parent
+{
+	
+	// Method Hiding ditandai dengan kata 'new'
+	public new void Greating()
+	{
+		Console.WriteLine("Bapak hiding");
+	} 
+}
+
+class Program 
+{
+	static void Main()
+	{
+		Parent p = new Child();
+		Child c = new Child();
+		
+		p.Greating();
+		c.Greating();
+	}
+}
---
+++ Overloading/Program.cs
@@ -0,0 +1,48 @@
+﻿class Calculator
+{
+	public double Add(double a, double b)
+	{
+		return a+b;
+	}
+	public float Add(float a, float b)
+	{
+		return a+b;
+	}
+	public int Add(int a, int b)
+	{
+		return a+b;
+	}
+	
+	//unlimited parameters
+	
+	public int Subtriction(params int[] data)
+	{
+		int result = 0;
+		
+		foreach (int i in data)
+		{
+			result +=i;
+			
+		}
+		return result;
+	}
+	
+	//overloading yang kurang tepat karena para meterr yang sama tipe data
+	// public void Add(int a, int b)
+	// {
+	// 	Console.WriteLine(a+b);
+	// }
+}
+
+class Program
+{
+	static void Main()
+	{
+		Calculator cal = new();
+		float fa = 1;
+		float fb = 2;
+		cal.Add(fa, fb); 
+		int result = cal.Subtriction(1,3,4,5,6,7,9);
+		Console.WriteLine(cal.Subtriction(result)); 
+	}
+}
