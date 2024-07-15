e23ae8110266406ffaa2a70c10ff9cd7b5ba7e11
notfound313 <marufmuh666@gmail.com>
2024-07-15 14:53:12 +0700

    ref_in_out

---
+++ V_Value_Type/Program.cs
@@ -0,0 +1,39 @@
+﻿//value type
+// reference type
+
+class Program
+{
+	static void Main()
+	{
+		int a = 3;
+		//reference type
+		Car car = new();
+		car.price = 3;
+		
+		//value type
+		Adder add = new();
+		add.Add(car);
+		Console.WriteLine(car.price);
+		
+	}
+}
+
+class Adder
+{
+	//value type
+	// public int Add(int x)
+	// {
+	// 	return x++;
+	// }
+	
+	//reference type
+	public int Add(Car x)
+	{
+		return x.price++;
+	}
+}
+
+class Car 
+{
+	public int price;
+}
---
+++ ref_in_out/Program.cs
@@ -0,0 +1,50 @@
+﻿
+class Program
+{
+	static void Main()
+	{
+		int a = 3;
+		
+		Car car = new();
+		car.price = 3;
+		
+		//ref
+		Adder add = new();
+		// add.Add(ref a);
+		Console.WriteLine(car.price);
+		
+		//out
+		// add.Add(out a);
+		
+		//
+		add.Add(in a);
+	}
+}
+
+class Adder
+{
+	//ref
+	// public int Add(ref int x)
+	// {
+	// 	return x++;
+	// }
+	
+	//out 
+	// public void Add(out int x)
+	// {
+	// 	x=6;
+		
+	// }
+	public void Add(in int x)
+	{
+		
+		
+	}
+	
+}
+	
+
+class Car 
+{
+	public int price;
+}
