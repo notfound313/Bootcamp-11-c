abd4d3f988ef189432c669209aa9fb945d7b9aab
notfound313 <marufmuh666@gmail.com>
2024-07-16 11:01:23 +0700

    generic constrain and method generic

---
+++ CalGen/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Numerics;
+
+//generic Method
+class Calculator
+{
+	public T Add<T>(T a , T b)where T : IAdditionOperators<T,T,T>
+	{
+		return a+b;
+		
+	}
+	public T Subtract<T>(T a , T b)where T : ISubtractionOperators<T,T,T>
+	{
+		return a-b;
+	}
+	public T Multiply<T>(T a , T b)where T : IMultiplyOperators<T,T,T>
+	{
+		return a*b;
+	}
+	public T Divide<T>(T a , T b)where T : IDivisionOperators<T,T,T>
+	{
+		return a/b;
+	}
+}
+
+
+class Program
+{
+	static void Main()
+	{
+		Calculator cal = new();
+		Console.WriteLine(cal.Add(1,9));
+		Console.WriteLine(cal.Add(1,9.8f));
+		Console.WriteLine(cal.Subtract(5.9M,8.0M));
+		
+		
+		
+	}
+}
---
+++ CalGenClass/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Numerics;
+
+//generic class
+class Calculator<T> where T : INumber<T>
+{
+	public T Add(T a , T b)
+	{
+		return a+b;
+		
+	}
+	public T  Subtract(T a , T b)
+	{
+		return a-b;
+		
+	}
+	public T Multiply(T a , T b)
+	{
+		return a*b;
+		
+	}
+	public T Divide(T a , T b)
+	{
+		return a/b;
+		
+	}
+}
+
+
+class Program
+{
+	static void Main()
+	{
+		Calculator<int> cal = new();
+		Console.WriteLine(cal.Add(1,9));
+		
+		
+		
+	}
+}
