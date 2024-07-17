edaee3c2ce2fbeda66b0a940843d297c1b5d3049
notfound313 <marufmuh666@gmail.com>
2024-07-17 15:08:36 +0700

    colletion

--- Collection/Program.cs
+++ Collection/Program.cs
@@ -20,12 +20,13 @@ class Program
 		List<string> arrString = new();
 		arrString.Add("halololo");
 		arrString.Add("jwaaahahaha"); 
+		
 		//Hashnet
 		HashSet<int> hashArrA = new() {1,2,3,4,5,6};
 		HashSet<int> hashArrB = new() {3,6,7,8,9};
 		
-		hashArrA.UnionWith(hashArrB);
-		hashArrA.IntersectWith(hashArrB);
+		// hashArrA.UnionWith(hashArrB);
+		// hashArrA.IntersectWith(hashArrB);
 		hashArrA.ExceptWith(hashArrB);
 		
 		HashSet<int> A = new() {1,2,3,4};
@@ -34,7 +35,53 @@ class Program
 		bool status = A.IsSupersetOf(B);
 		status= B.IsSubsetOf(A);
 		
-		Console.WriteLine(arr);
+		// foreach(int i in hashArrA)
+		// {
+		// 	Console.WriteLine(i);
+		// }
+		
+		Queue<int> ints = new Queue<int>();
+		ints.Enqueue(3);
+		ints.Enqueue(2);
+		ints.Enqueue(1);
+		ints.Enqueue(9);
+		int rest = ints.Dequeue();//3
+		int pek = ints.Peek();//2
+		int resti = ints.Dequeue();//2
+		
+		Stack<int> stack =new();
+		stack.Push(2);
+		stack.Push(3);
+		stack.Push(5);
+		stack.Pop();
+		stack.Pop();
+		
+		
+		
+		//Dicttionaray
+		
+		Dictionary<int,string> dict = new();
+		
+		dict.Add(1,"jaja");
+		dict.Add(3,"juju");
+		dict.Add(2,"lalal");
+		
+		bool stats = dict.TryGetValue(3,out string resy);
+		if(stats)
+		{
+			Console.WriteLine(resy);
+		}
+		dict[3] = "lllknk";
+		
+		Console.WriteLine(dict[3]);
+		Console.WriteLine(stats);
+		Console.WriteLine(dict.Keys);
+
+		
+		
+		
+		
+		
 		
 	
 		
