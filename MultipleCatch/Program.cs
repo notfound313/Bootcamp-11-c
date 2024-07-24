778f83cb1f40cc0c28b6a2cd1aa151ac60783f28
notfound313 <marufmuh666@gmail.com>
2024-07-24 10:09:55 +0700

    multiple catch

---
+++ MultipleCatch/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+	static void Main()
+	{
+		try
+		{
+			// string angka= "9k";
+			// int value = int.Parse(angka);
+			
+			int[] arr = null;
+			Console.WriteLine(arr[8]);
+			
+			int[] arr2 = {1,2,3,4,5};
+			Console.WriteLine(arr2[8]);
+		}
+		catch (NullReferenceException ex)
+		{
+			Console.WriteLine(ex.Message);
+		}
+		catch (IndexOutOfRangeException ex)
+		{
+			Console.WriteLine(ex.Message);
+		}
+		catch(Exception e)
+		{
+			Console.WriteLine(e.Message);
+			
+		}
+	}
+	
+}
