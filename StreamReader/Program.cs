58c015ce4e1aec3444cc648aa417c615c0ce6de3
notfound313 <marufmuh666@gmail.com>
2024-07-30 14:56:04 +0700

    streamreader and streamwriter

---
+++ StreamReader/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+	static void Main()
+	{
+		string path = @".\myTest.txt";
+		using(StreamReader sr = new(path) )
+		{
+			string text = sr.ReadLine();
+			if(text is not null)
+			{
+				Console.WriteLine(text);
+			}
+		}
+	}
+	
+}
---
+++ StreamWriter/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+	static void Main()
+	{
+		string path = @".\myTest.txt";
+		using(StreamWriter sw = new StreamWriter(path))
+		{
+			sw.WriteLine("Hello Uhuuuuiiii");
+			
+		}
+	}
+	
+}
